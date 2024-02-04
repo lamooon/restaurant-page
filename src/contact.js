1964f387605e29f1b691b5e7fd9ee7f2e92678ec
lamooon <111581657+lamooon@users.noreply.github.com>
2024-02-04 22:03:55 +0900

    Done menu.js

---
+++ src/contact.js
@@ -0,0 +1,10 @@
+
+
+
+function contact() {
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+    return contact;
+}
+
+export default contact;
---
+++ src/menu.js
@@ -0,0 +1,33 @@
+function createMenu(name, price) {
+    const item = document.createElement('div');
+
+    const title = document.createElement('h2');
+    title.textContent = name;
+
+    const priceTag = document.createElement('h3');
+    priceTag.textContent = `$${price}`;
+
+    const image = document.createElement('img');
+    image.src = `image/${name.toLowerCase()}.jpg`;
+    image.alt = name;
+
+    item.appendChild(image);
+    item.appendChild(title);
+    item.appendChild(priceTag);
+
+    return item;
+}
+
+function menu() {
+    const main = document.createElement('div');
+    main.classList.add('menu');
+    const friedRice = createMenu("Fried rice", 9.99);
+    const pho = createMenu("Pho", 7.99);
+    const sushi = createMenu("Sushi", 14.99);
+    main.appendChild(friedRice);
+    main.appendChild(pho);
+
+    return main;
+}
+
+export default menu;
